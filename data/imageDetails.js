8d804f13b3331a5b6c505d9201f7fafa18c56b96
Michle99 <sammiyonyx@gmail.com>
2023-11-10 23:11:01 -0500

    convert imageDetails to nature details.

--- data/imageDetails.js
+++ data/imageDetails.js
@@ -6,5 +6,87 @@ const imageDetails = {
     5: { id: 5, description: 'Abstract art with vibrant colors.' },
     6: { id: 6, description: 'Abstract art with vibrant colors.' },
 };
+
+// Replace the imageDetails
+const natureDescriptions = [
+    {
+        id: 1,
+        category: "Sea",
+        description: "The calm sea stretches as far as the eye can see, reflecting the brilliant hues of the sky during sunset."
+    },
+    {
+        id: 2,
+        category: "Ocean",
+        description: "The vast ocean whispers tales of ancient times, its waves gently caressing the sandy shores."
+    },
+    {
+        id: 3,
+        category: "Land",
+        description: "Endless stretches of green land unfold, adorned with colorful wildflowers and dancing butterflies."
+    },
+    {
+        id: 4,
+        category: "Beach",
+        description: "A pristine beach invites with its soft, golden sands and the rhythmic lullaby of rolling waves."
+    },
+    {
+        id: 5,
+        category: "Mountain",
+        description: "Towerous mountains rise majestically, their peaks lost in the embrace of swirling clouds."
+    },
+    {
+        id: 6,
+        category: "Weather",
+        description: "A clear sky paints a canvas of blue, interrupted only by the occasional fluffy clouds drifting lazily."
+    },
+    {
+        id: 7,
+        category: "Sunset",
+        description: "The sun bids adieu, painting the sky in warm hues of orange and pink, casting a serene glow across the landscape."
+    },
+    {
+        id: 8,
+        category: "Forest",
+        description: "A dense forest beckons, its lush canopy creating a world of shadows and sunlight dancing on the forest floor."
+    },
+    {
+        id: 9,
+        category: "Lake",
+        description: "A tranquil lake mirrors the surrounding beauty, reflecting the surrounding trees and the clear blue sky."
+    },
+    {
+        id: 10,
+        category: "Storm",
+        description: "Dark clouds gather, thunder rumbles, and lightning streaks across the sky as nature unleashes its powerful and captivating storm."
+    },
+    {
+        id: 11,
+        category: "Meadow",
+        description: "An open meadow, filled with a carpet of wildflowers, invites you to bask in the gentle warmth of the sun."
+    },
+    {
+        id: 12,
+        category: "Desert",
+        description: "Endless sand dunes stretch into the horizon, the hot desert sun casting mesmerizing patterns on the golden landscape."
+    },
+    {
+        id: 13,
+        category: "Rainforest",
+        description: "The air is thick with humidity as vibrant foliage and exotic creatures create a symphony of life in the heart of the rainforest."
+    },
+    {
+        id: 14,
+        category: "Canyon",
+        description: "A grand canyon reveals the Earth's history through its layered rock formations, carved by the patient hands of time."
+    },
+    {
+        id: 15,
+        category: "Island",
+        description: "A secluded island paradise, surrounded by crystal-clear waters, offers a peaceful retreat from the hustle and bustle of life."
+    }
+];
+
+console.log(natureDescriptions);
+
   
 module.exports = imageDetails;
